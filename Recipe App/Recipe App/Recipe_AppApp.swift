3b558b4a702b808f409fe0bd24498b387a7d93bd
Galina Abdurashitova <g.m.abdurashitova@gmail.com>
2024-04-08 19:20:25 +0100

    Main

---
+++ Recipe App/Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Galina Abdurashitova on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
