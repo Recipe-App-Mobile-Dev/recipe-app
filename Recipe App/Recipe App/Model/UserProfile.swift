de86375ce519a69c69f054acb80582eae5b7e51b
Jin Mizuno <bump.lk_k.1215@icloud.com>
2024-04-27 01:44:17 +0100

    Create User Profile

---
+++ Recipe App/Recipe App/Model/UserProfile.swift
@@ -0,0 +1,14 @@
+//
+//  UserProfile.swift
+//  Recipe App
+//
+//  Created by Jin Mizuno on 27/04/2024.
+//
+
+import Foundation
+
+struct UserProfile: Codable {
+    var uid: String
+    var name: String
+    var icon: String
+}
