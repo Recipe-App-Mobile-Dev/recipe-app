8004c5a4ad17eac6609c58f1035bc6911277b3d5
Jin Mizuno <bump.lk_k.1215@icloud.com>
2024-04-30 23:05:29 +0100

    Add user profile page

---
+++ Recipe App/Recipe App/SharedView/ProfileCardView.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileCardView.swift
+//  Recipe App
+//
+//  Created by Jin Mizuno on 30/04/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProfileCardView: View {
+    @State var profile: UserProfile
+    
+    var body: some View {
+        HStack {
+            LoadImageView(imageName: "profiles/" + profile.icon)
+                .scaledToFill()
+                .clipShape(Circle())
+                .frame(width: 60, height: 60)
+                .padding(.leading)
+            VStack(alignment: .leading) {
+                Text(profile.name)
+                    .font(.headline)
+            }
+            .padding(.leading)
+            Spacer()
+        }.padding()
+    }
+}
+
+struct ProfileCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileCardView(profile: AuthModel(testProfile: true).profile)
+    }
+}
